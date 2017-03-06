7a473d8bbb53df3a7dabf1e187f698c19dfacc4c
rjbgoudie <rjbgoudie@users.noreply.github.com>
2017-03-06 10:53:56 +0000

    Document the example R files

--- submission-scripts/r-single-core-array/myrfile.R
+++ submission-scripts/r-single-core-array/myrfile.R
@@ -2,9 +2,16 @@
 task_id_string <- Sys.getenv("SLURM_ARRAY_TASK_ID")
 task_id <- as.numeric(task_id_string)
 
+# set a different random seed for each task in the array
+# task 1 will use random seed 425, task 2 will use 3453 etc
 seed <- c(425, 3453, 223, 232)[task_id]
 set.seed(seed)
+
+# draw 100 standard normals
 samples <- rnorm(100)
 
+# save the standard normals in a file
+# remember to give each task array item a diffent filename, otherwise they will
+# obviously all overwrite each other
 filename <- paste0("samples_", task_id, ".rds")
 saveRDS(samples, file = filename)
--- submission-scripts/r-single-core/myrfile.R
+++ submission-scripts/r-single-core/myrfile.R
@@ -1,5 +1,9 @@
+# set random seed
 set.seed(425)
+
+# draw 100 standard normals
 samples <- rnorm(100)
 
+# save the standard normals to a file
 filename <- paste0("samples.rds")
 saveRDS(samples, file = filename)
